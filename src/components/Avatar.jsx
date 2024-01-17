0b4dabe01e7d1c7140ded183fe20f32d3caa1e88
p-nanaware <pnanaware.umassd.edu>
2024-01-17 01:45:48 -0500

    update:portfolio url

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -255,7 +255,7 @@ export function Avatar(props) {
       <Html distanceFactor={2} fullscreen={true} position={[0, 1, 0]}>
         <div className="flex flex-col items-center justify-center">
           <a
-            href="https://pranavnanaware.github.io/"
+            href="https://pranavn-portfolio.vercel.app/"
             className="relative inline-flex items-center justify-center px-4 py-4 overflow-hidden font-mono font-medium tracking-tighter text-white bg-gray-800 rounded-lg group"
           >
             <span className="absolute w-0 h-0 transition-all duration-500 ease-out bg-blue-500 rounded-full group-hover:w-56 group-hover:h-56"></span>
