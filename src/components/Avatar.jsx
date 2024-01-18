6fc17575e40700cf5f674b7c85fe1f2ac8b2edb5
p-nanaware <pnanaware.umassd.edu>
2024-01-17 23:15:15 -0500

    greeting update

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -267,7 +267,13 @@ export function Avatar(props) {
           <a className="relative  inline-flex items-center justify-center px-4 py-4 overflow-hidden font-mono font-medium tracking-tighter text-white bg-gray-800 rounded-lg group ">
             <span className="absolute w-0 h-0 transition-all duration-500 ease-out bg-blue-500 rounded-full group-hover:w-56 group-hover:h-56"></span>
             <span className="absolute inset-0 w-full h-full -mt-1 rounded-lg opacity-30 bg-gradient-to-b from-transparent via-transparent to-gray-700"></span>
-            <span className="relative" onClick={() => play()}>
+            <span
+              className="relative"
+              onClick={() => {
+                play();
+                setAnimation("Greeting");
+              }}
+            >
               Elevator Pitch
             </span>
           </a>
